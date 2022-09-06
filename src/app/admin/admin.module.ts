f3759f7591a1ea2a8079a03df1c3093221b81059
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-09-06 17:10:57 +0200

    x

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -14,6 +14,7 @@ import { AdminComponent } from './admin.component';
     CommonModule,
     AdminRoutingModule,
     SharedModule
-  ]
+  ],
+  bootstrap:[AdminComponent]
 })
 export class AdminModule { }
