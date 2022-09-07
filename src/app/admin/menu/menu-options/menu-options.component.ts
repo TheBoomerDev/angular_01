32eb14829e185f7a1ec4b04fa153bff0cb859fc9
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-09-07 19:47:45 +0200

    👌 IMPROVE: Menu + Json Import

--- src/app/admin/menu/menu-options/menu-options.component.ts
+++ src/app/admin/menu/menu-options/menu-options.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import menu from './menu.json';
+
 @Component({
   selector: 'menu-options',
   templateUrl: './menu-options.component.html',
   styleUrls: ['./menu-options.component.scss']
 })
 export class MenuOptionsComponent implements OnInit {
+
+  menuOptions:any = menu
 
   constructor() { }
 
