acc33116dbe9adb2a732a7d544a1400f4406c071
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-09-07 20:08:22 +0200

    👌 IMPROVE: Admin Routing -> New Features

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,6 +4,12 @@ import { AdminComponent } from './admin.component';
 import { ProfileComponent } from './screens/profile/profile.component';
 
 const children: Routes = [
+  { path: 'links',
+    loadChildren: () => import('./screens/links/links.module').then(m => m.LinksModule)},
+  { path: 'historical',
+    loadChildren: () => import('./screens/historical/historical.module').then(m => m.HistoricalModule)},
+  { path: 'payment',
+    loadChildren: () => import('./screens/payment/payment.module').then(m => m.PaymentModule)},
   { path: 'profile', component: ProfileComponent },
   { path: '**', redirectTo: 'profile' }
 ];
