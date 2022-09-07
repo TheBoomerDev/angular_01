42db6080c6e4cc38e23d1168aeb35790c97d9e7c
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-09-07 16:30:43 +0200

    👌 IMPROVE: Inicio + Botones de Acceso

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  goLogin = () =>{
+    this.router.navigate(['/auth/login'])
+  }
+
+  goRegister = () =>{
+    this.router.navigate(['/auth/register'])
   }
 
 }
