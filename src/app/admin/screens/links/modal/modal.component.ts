55fdf18b3b5d7f7c6ecf2e69072446b107d5af7a
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-09-09 18:02:18 +0200

    👌 IMPROVE: Dialog -> Recoger Datos + Diferenciar New/Edit

--- src/app/admin/screens/links/modal/modal.component.ts
+++ src/app/admin/screens/links/modal/modal.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { LinkModel } from 'src/app/common/models/link.model';
 
 @Component({
   selector: 'modal-links',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
 
-  constructor() { }
+  title:string = 'NUEVO'
+  item:LinkModel = new LinkModel()
 
-  ngOnInit(): void {
+  constructor(public dialogRef: MatDialogRef<ModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+
+      console.log('DATA Modal', data)
+
+      if (data){
+        const item = data['item']
+        if (item){
+          this.item = item
+          this.title = 'Editar'
+        }
+      }
+
+  }
+
+  doClose = () => {
+    this.dialogRef.close('CERRADO')
   }
 
 }
